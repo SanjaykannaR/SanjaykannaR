457fffeb531c1d23af6ca763f339c4e899360dc8
SanjaykannaR <165680113+SanjaykannaR@users.noreply.github.com>
2024-08-12 18:27:08 +0530

    adding Boolean logical operation

--- Main.kt
+++ Main.kt
@@ -4,20 +4,20 @@ fun main() {
     println(123 + 456 * 789)
     println(1 + 2 * 3 + 4 * 5 + 6 * 7 + 8 * 9)
     println((1 + 2) * 3 + 4 * 5 + 6 * (7 + 8) * 9)
-        println(13530.0 / 1.23)
-        val scoreLevel1 = 79
-        val scoreLevel2 = 92
-        val scoreLevel3 = 86
-            val totalScore = scoreLevel1 + scoreLevel2 + scoreLevel3
-            val averageScore = totalScore / 3
-            println(totalScore)
-            println(averageScore)
-                val boostMultiplier= 4
-                val scoreBoost= totalScore * boostMultiplier
-                val finalBoostedScore = totalScore+scoreBoost
-                println(scoreBoost)
-                println(finalBoostedScore)
-    println(5+3*8)
+    println(13530.0 / 1.23)
+    val scoreLevel1 = 79
+    val scoreLevel2 = 92
+    val scoreLevel3 = 86
+    val totalScore = scoreLevel1 + scoreLevel2 + scoreLevel3
+    val averageScore = totalScore / 3
+    println(totalScore)
+    println(averageScore)
+    val boostMultiplier = 4
+    val scoreBoost = totalScore * boostMultiplier
+    val finalBoostedScore = totalScore + scoreBoost
+    println(scoreBoost)
+    println(finalBoostedScore)
+    println(5 + 3 * 8)
 
 
     var s1 = "My "
@@ -39,7 +39,31 @@ fun main() {
     println(myString2)
 
 
-
     val num = 10
     println("Result is $num + 20")
+
+    //Logical operations: Boolean
+    val finishedHomework = true // or true
+    val cleanedRoom = true // or false
+    val canPlayGames = finishedHomework && cleanedRoom
+    println(canPlayGames)
+
+    val carCleaned = false // or true
+    val grassCut = true // or false
+    val canGoToCinema = carCleaned || grassCut
+    println(canGoToCinema)
+
+    val percent = 47
+    val incorrect = percent < 0 || percent < 100
+    println(incorrect)
+
+
+    val text = """
+    |Tell me and I forget.
+    |Teach me and I remember.
+    |Involve me and I learn.
+    |(Benjamin Franklin)
+    """.trimMargin()
+    println(text)
 }
+
